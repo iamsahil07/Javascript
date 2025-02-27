02f2bfad0d8667959544b06bf362d1f5a2102869
Sahil <104717976+iamsahil07@users.noreply.github.com>
2025-02-27 04:26:49 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+// console.log(typeof score); => string
+
+let variableInNumber = Number(score)
+
+// console.log(typeof variableInNumber); => number
+
+// "33" => 33
+// "33abc" => NaN
+// "Sahil" => true
+
+let isLoggedIn =  "sahil"
+
+// console.log(typeof isLoggedIn); => String
+
+let variableInboolean = Boolean(isLoggedIn)
+
+// console.log(typeof variableInboolean); => boolean
+
+// 1 => true ; 0 => false
+// "" => false
+// "sahil" => true
+
+let someNumber = 33
+
+// console.log(typeof someNumber); => number
+
+let variableInString = String(someNumber)
+
+// console.log(typeof variableInString); => string
+
