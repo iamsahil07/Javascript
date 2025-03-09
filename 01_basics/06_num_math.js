41680a5615fb73c373d3c644b3d57155ba535861
Sahil <104717976+iamsahil07@users.noreply.github.com>
2025-03-09 04:46:50 +0000

    into to numers and maths

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,27 @@
+const score = 400
+//  console.log(score);
+const balance  = new Number(400)
+//  console.log(balance);
+//  console.log(balance.toString().length);
+
+//  console.log(balance.toFixed(3)); // 400.000 (returns scientific notation)
+
+const otherNum = 123.4566
+//  console.log(otherNum.toPrecision(4)); // 123.4
+
+const m = 1000000
+//  console.log(m.toLocaleString('en-IN')); // can be used to count no. of zeros
+
+
+//  ******************  Maths  *****************  //
+
+//  console.log(Math.round(4.6));
+
+console.log(Math.random()); // returns any random value which returns greater than or equal to 0 and less than 1
+// console.log((Math.random() *10)+1);
+// console.log(Math.floor((Math.random() * 10)+1));
+
+const max = 33
+const min = 29
+
+console.log(Math.floor((Math.random()*(max - min +1))+min)); //  returns values between min and max
