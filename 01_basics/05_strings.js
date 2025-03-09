d2a68635623cd88f12fe3092d12b2a0319879dc8
Sahil <104717976+iamsahil07@users.noreply.github.com>
2025-03-09 03:10:15 +0000

    tested

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "sahil"
+const age = 22
+
+// console.log(name + age);
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String("call-of-duty");
+console.log(gameName[0]);
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("-"));
+
+console.log(gameName.substring(2,6));
+
