8f158311db1e71b2fc78dc534a01f8163801da61
Sahil <104717976+iamsahil07@users.noreply.github.com>
2025-03-10 04:55:06 +0000

    added info for splice and slice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,7 +2,7 @@
 
 const arr = [1,2,3,4,5,6]
 const arr2 = [2,4,6,5]
-
+/*
 arr.push(8);
 console.log(arr);
 arr.pop()
@@ -19,12 +19,13 @@ console.log(arr.includes(9))
 const arr3 = arr.join()
 console.log(arr)
 console.log(arr3)
-
+*/
 /*  ********  slice, splice  *********  */
-console.log("A" , arr)
+console.log("A" , arr) // [1,2,3,4,5,6]
 const myn1 = arr.slice(1, 3)
-console.log(myn1)  // [1,2]
+console.log(myn1)  // [2,3]
+console.log("B", arr) // [1,2,3,4,5,6]
 
-console.log("B", arr)
-const myn2 = arr.splice(1,3)
-console.log(myn2)  // [1,2,3]
+const myn2 = arr.splice(1,3) // splce removes the elements from the 1 to 3 and update the array
+console.log(myn2)  // [2,3,4], elements removed from array
+console.log("c", arr) // [1,5,6], updated array
