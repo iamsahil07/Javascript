dfc4672c9a30c09d01b6cb681ce1c3f27c9a22f8
Sahil <104717976+iamsahil07@users.noreply.github.com>
2025-03-21 15:27:24 +0000

    into to forEach

---
+++ 06_iteration/three.js
@@ -0,0 +1,40 @@
+// forEach : it is an iterative method which an provided callback() 
+// function for each array element
+
+const arr = ["js", "py", "cpp", "ruby"];
+
+arr.forEach(function (val) {
+  // console.log(val);
+});
+
+arr.forEach((lang) => {
+  // console.log(lang);
+});
+
+function printMe(item) {
+  // console.log(item);
+}
+
+arr.forEach(printMe);
+
+arr.forEach((value, index, arr) => {
+  // console.log(value,index,arr);
+});
+
+const myCoding = [
+  {
+    programmingLanguage: "python",
+    programmingFile: "py",
+  },
+  {
+    programmingLanguage: "ruby",
+    programmingFile: "rb",
+  },
+  {
+    programmingLanguage: "javascript",
+    programmingFile: "js",
+  },
+];
+myCoding.forEach((lang) => {
+  console.log(lang.programmingFile);
+});
